dcd9770964b4a6623184d49bce2be9513e67f210
MMOSimca <mmosimca@gmail.com>
2016-11-08 15:44:11 -0500

    Added Shar'thos bonus loot spell.

--- Constants.lua
+++ Constants.lua
@@ -725,13 +725,13 @@ private.RAID_BOSS_BONUS_SPELL_ID_TO_NPC_ID_MAP = {
     [227131] = 99929, -- 7.0 Raid World Boss - Bonus Roll Prompt (Flotsam)
     [227133] = 108829, -- 7.0 Raid World Boss - Bonus Roll Prompt (Levantus)
     [227135] = 107023, -- 7.0 Raid World Boss - Bonus Roll Prompt (Nithogg)
+    [227136] = 108678, -- 7.0 Raid World Boss - Bonus Roll Prompt (Shar'thos)
     [227138] = 102075, -- 7.0 Raid World Boss - Bonus Roll Prompt (Withered J'im)
     --[] = 109943, -- 7.0 Raid World Boss - Bonus Roll Prompt (Ana-Mouz)
     --[] = 109331, -- 7.0 Raid World Boss - Bonus Roll Prompt (Calamir)
     --[] = 106981, -- 7.0 Raid World Boss - Bonus Roll Prompt (Captain Hring)
     --[] = 108879, -- 7.0 Raid World Boss - Bonus Roll Prompt (Humongris)
     --[] = 110321, -- 7.0 Raid World Boss - Bonus Roll Prompt (Na'zak the Fiend)
-    --[] = 108678, -- 7.0 Raid World Boss - Bonus Roll Prompt (Shar'thos)
 
     -----------------------------------------------------------------------
     -- Hellfire Citadel
